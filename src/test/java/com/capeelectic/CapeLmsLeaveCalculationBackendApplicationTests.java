afa9dac03e19c238b2d123b1708746c9adf5daa7
thirumoorthy2005 <79498283+thirumoorthy2005@users.noreply.github.com>
2022-10-19 11:33:32 +0530

    firstcommit

---
+++ src/test/java/com/capeelectic/CapeLmsLeaveCalculationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capeelectic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapeLmsLeaveCalculationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
